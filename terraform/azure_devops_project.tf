213216e1379c1aa0f46f83435798509c609e398a
Adan Daniel <axd3@cbord.com>
2024-03-28 12:01:57 -0400

    changes

---
+++ terraform/azure_devops_project.tf
@@ -0,0 +1,28 @@
+resource "azuredevops_project" "formula_monks_azdvps" {
+  name               = "formula_monks_web"
+  visibility         = "public"
+  version_control    = "Git"
+  work_item_template = "Basic"
+  description        = "Managed by Terraform"
+    features = {
+    "boards"       = "enabled"
+    "repositories" = "enabled"
+    "pipelines"    = "enabled"
+    "testplans"    = "disabled"
+    "artifacts"    = "disabled"
+  }
+}
+
+
+resource "azuredevops_build_definition" "pipeline" {
+  project_id = azuredevops_project.formula_monks_azdvps.id
+  name       = "ExamplePipeline"
+  path       = "\\"
+
+  repository {
+    repo_type   = "GitHub"
+    repo_id     = "adamdaniel2993/formunla-monks"
+    branch_name = "main"
+    yml_path    = "azure-pipelines.yml"
+  }
+}
--- terraform/providers_backends.tf
+++ terraform/providers_backends.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azuredevops = {
       source = "microsoft/azuredevops"
-      version = ">= 0.1.0"
+      version = ">= 0.2.0"
     }
   }
 }
@@ -14,6 +14,10 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+}
+
+provider "azuredevops" {
+  use_msi = true
 }
 
 terraform {
