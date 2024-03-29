a8cf97d7573d354a31bc46d63e071b5e814c8f0d
Adan Daniel <axd3@cbord.com>
2024-03-29 09:30:50 -0400

    testing new image

--- formulatest/pages/views.py
+++ formulatest/pages/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return HttpResponse("Hello, Formula Monks this is adan daniel test!")
+    return HttpResponse("Hello, Formula Monks this is adan daniel test! I hope you like it")
 
 # Create your views here.
