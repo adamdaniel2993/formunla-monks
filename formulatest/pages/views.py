45517f7806d88664a57b88add89bcc4f00d274ae
Adan Daniel <axd3@cbord.com>
2024-03-29 11:53:42 -0400

    tesitng group vars

--- formulatest/pages/views.py
+++ formulatest/pages/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return HttpResponse("Hello, Formula Monks this is adan daniel test! I hope you like it, this is a new image trigger by azure devops")
+    return HttpResponse("Hello, Formula Monks this is adan daniel test! I hope you like it, this is a new image trigger by azure devops, using group variables")
 
 # Create your views here.
