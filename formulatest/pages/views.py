85f3c82f74c80568bd465d6b5ea91c4636a9ad70
Adan Daniel <axd3@cbord.com>
2024-03-29 11:32:28 -0400

    updating app to trigger pipeline new image creation

--- formulatest/pages/views.py
+++ formulatest/pages/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return HttpResponse("Hello, Formula Monks this is adan daniel test! I hope you like it")
+    return HttpResponse("Hello, Formula Monks this is adan daniel test! I hope you like it, this is a new image trigger by azure devops")
 
 # Create your views here.
